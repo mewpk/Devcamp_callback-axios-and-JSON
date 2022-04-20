d70ea739e16ffe08fbc57b397b9977a6a13e453e
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-04-20 14:23:06 +0700

    save

---
+++ script.js
@@ -0,0 +1,20 @@
+// debugger;
+// var fruits = ["apple", "banana", "mango"]
+
+// const addFruits = (fruit, callback) => {
+//     setTimeout(() => {
+//         fruits.push(fruit);
+//         console.log(`add ${fruit} Success`)
+//         callback()
+//     }, 500)
+// }
+
+// addFruits("pineapple", () => {
+//     setTimeout(() => {
+//         console.log(fruits)
+//     }, 200)
+// })
+
+
+
+
